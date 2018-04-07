69c22c78c763f42f9349a4bd8f5844e95eebb3f5
enigma-phi <29843841+enigma-phi@users.noreply.github.com>
2018-04-07 16:01:33 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,8 @@ Several constants used in the project
 #BATCH_SIZE = 1
 #RNN_SIZE = 0
 ENCODING = 'UTF-8'
+# JARED fill this in with the characters you are using and the 
+# number of characters 
+# VOCAB_SIZE = ...
+# VOCAB = ...
+EMBED_SIZE = 256
