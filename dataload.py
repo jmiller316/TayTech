321b2902ac2aeb826837674cbebcf5c0285487bc
jaredpolw <31479843+jaredpolw@users.noreply.github.com>
2018-04-10 13:57:53 -0500

    Add files via upload

---
+++ dataload.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 21:18:54 2018
+
+@author: jared
+"""
+import numpy as np
+import tensorflow as tf
+from utils import *
+import codecs
+import os
+from config import *
+
+def input_load():
+    #creates vocab conversion dictionaries
+    char2idx, idx2char = create_vocab()
+    fpaths, text_lengths, texts = [], [], []
+    
+    transcript = os.path.join("/data/Garret", 'transcript.csv')
+    lines = codecs.open(transcript, 'r', 'utf-8').readlines()
+    line_number = 0
+    for line in lines:
+        line_number += 1
+        text, fname = line.strip().split("$")
+        if not fname:
+            fpath = os.path.join("/data/Garret","G" + line_number + ".wav")
+        else:
+            fpath = os.path.join("/data/Garret",fname + ".wav")
+            
+        fpaths.append(fpath)                                        #queue of fpaths containing all the wavfiles
+
+        text = normalize_text(text) + "$"  # E: EOS
+        text = [char2idx[char] for char in text]
+        text_lengths.append(len(text))
+        texts.append(np.array(text, np.int32).tostring())           #queue of converted transcript text lines
+        
+    transcript = os.path.join("/data/Collin", 'transcript.csv')
+    lines = codecs.open(transcript, 'r', 'utf-8').readlines()
+    line_number = 0
+    for line in lines:
+        line_number += 1
+        text, fname = line.strip().split("$")
+        if not fname:
+            fpath = os.path.join("/data/Collin","C" + line_number + ".wav")
+        else:
+            fpath = os.path.join("/data/Collin", fname + ".wav")
+            
+        fpaths.append(fpath)                                        #queue of fpaths containing all the wavfiles
+
+        text = normalize_text(text) + "$"  # E: EOS
+        text = [char2idx[char] for char in text]
+        text_lengths.append(len(text))
+        texts.append(np.array(text, np.int32).tostring())           #queue of converted transcript text lines
+        
+    transcript = os.path.join("/data/David", 'transcript.csv')
+    lines = codecs.open(transcript, 'r', 'utf-8').readlines()
+    line_number = 0
+    for line in lines:
+        line_number += 1
+        text, fname = line.strip().split("$")
+        if not fname:
+            fpath = os.path.join("/data/David","D" + line_number + ".wav")
+        else:
+            fpath = os.path.join("/data/David", fname + ".wav")
+            
+        fpaths.append(fpath)                                        #queue of fpaths containing all the wavfiles
+
+        text = normalize_text(text) + "$"  # E: EOS
+        text = [char2idx[char] for char in text]
+        text_lengths.append(len(text))
+        texts.append(np.array(text, np.int32).tostring())           #queue of converted transcript text lines
+        
+    return fpaths, text_lengths, texts
+
+def get_batch():
+    with tf.device('/device:GPU:0'): #this uses your primary gpu use ('/cpu:0') to use your cpu instead
+        
+        fpaths, text_lengths, texts = input_load()
+        maxlen, minlen = max(text_lengths), min(text_lengths)
+        
+        num_batch = len(fpaths)
+        
+        fpaths = tf.convert_to_tensor(fpaths)
+        text_lengths = tf.convert_to_tensor(text_lengths)
+        texts = tf.convert_to_tensor(texts)
+        
+        fpath, text_length, text = tf.train.slice_input_producer([fpaths, text_lengths, texts], shuffle=True) #forms queues from lists
+        
+        fname, mel, mag = tf.py_func(create_spectrograms, [fpath], [tf.string, tf.float32, tf.float32])
+        
+        fname.set_shape(())
+        text.set_shape((None,))
+        mel.set_shape((None, n_mels*hp.r))
+        mag.set_shape((None, n_fft//2+1))
+        
+        # Parse
+        text = tf.decode_raw(text, tf.int32)  # (None,)
+        
+        _, (texts, mels, mags, fnames) = tf.contrib.training.bucket_by_sequence_length(
+                                            input_length=text_length,
+                                            tensors=[text, mel, mag, fname],
+                                            batch_size=16,
+                                            bucket_boundaries=[i for i in range(minlen + 1, maxlen - 1, 20)],
+                                            num_threads=16,
+                                            capacity= batch_size * 4,
+                                            dynamic_pad=True)
+        
+        return texts, mels, mags, fnames, num_batch
--- utils.py
+++ utils.py
@@ -3,36 +3,107 @@ utils.py
 
 Miscellaneous functions that we will need.
 """
-import tensorflow as tf
+import librosa
+import numpy as np
+from scipy import signal
+import copy
+from config import *
 
+"""
+attention
+"""
 
-def learning_rate_decay(init_lr, global_step, warmup_steps=4000.0):
-    """
-    Learning_rate_decay.
+"""
+pre-net
+"""
 
-    The learning rate decay starts from 0.001 and is reduced to 0.0005, 0.0003, 
-    and 0.0001 after 500K, 1M, and 2M global steps respectively.
+"""
+text input edit
+"""
+def create_vocab():
+    char2idx = {char: idx for idx, char in enumerate(VOCAB)}
+    idx2char = {idx: char for idx, char in enumerate(VOCAB)}
+    return char2idx,idx2char
 
-    While the rate is hardcoded in here, it can be changed to the Naome from 
-    tensor2tensor if it does not work well. 
-    """
-    """# Naomi from tensor2tensor
-    step = tf.cast(global_step + 1, dtype=tf.float32)
-    return init_lr * warmup_steps**0.5 * tf.minimum(step * warmup_steps**(-1.5), step**(-0.5))"""
-    # Constants for the learning rate
-    r1 = tf.constant(0.001)
-    r2 = tf.constant(0.0005)
-    r3 = tf.constant(0.0003)
-    r4 = tf.constant(0.0001)
-    l1 = tf.constant(500000)
-    l2 = tf.constant(1000000)
-    l3 = tf.constant(2000000)
+def normalize_text(text):
+    #room for more normalization depending on transcript data
+    text = text.lower().replace(",",".")
+    return text
+"""
+audio edit
+"""
+def wav2spectrograms(fpath):
     
-    # Compare the values to determine the learning rate
-    if tf.less(global_step, l1):
-        return r1
-    if tf.greater_equal(global_step, l1) and tf.less(global_step, l2):
-        return r2
-    if tf.greater_equal(global_step, l2) and tf.less(global_step, l3):
-        return r3
-    return r4
+    y, sample_rate = librosa.load(fpath, sample_rate=sample_rate)                   #load the wav file
+    
+    y, _ = librosa.effects.trim(y)                          #trimming
+    
+    y = np.append(y[0], y[1:] - preemphasis * y[:-1])    #noise reduction technique
+    
+    linear = librosa.stft(y=y,                              #short time fourier transform to get the linear spectrogram
+                          n_fft=n_fft,
+                          hop_length=hop_length,
+                          win_length=win_length)
+    
+    mag = np.abs(linear)
+    
+    mel_basis = librosa.filters.mel(sample_rate, n_fft, n_mels)  # (n_mels, 1+n_fft//2)
+    mel = np.dot(mel_basis, mag)  # (n_mels, t)
+    
+    mel = 20 * np.log10(np.maximum(1e-5, mel))
+    mag = 20 * np.log10(np.maximum(1e-5, mag))
+
+    # normalize
+    mel = np.clip((mel - ref_db + max_db) / max_db, 1e-8, 1)
+    mag = np.clip((mag - ref_db + max_db) / max_db, 1e-8, 1)
+
+    # Transpose
+    mel = mel.T.astype(np.float32)  # (T, n_mels)
+    mag = mag.T.astype(np.float32)  # (T, 1+n_fft//2)
+    
+    return mel,mag
+
+def spectrogram2wav(spectrogram):
+    '''# Generate wave file from spectrogram'''
+    # transpose
+    spectrogram = spectrogram.T
+
+    # de-noramlize
+    spectrogram = (np.clip(spectrogram, 0, 1) * max_db) - max_db + ref_db
+
+    # to amplitude
+    spectrogram = np.power(10.0, spectrogram * 0.05)
+
+    # wav reconstruction
+    wav = griffin_lim(spectrogram)
+
+    # de-preemphasis
+    wav = signal.lfilter([1], [1, -preemphasis], wav)
+
+    # trim
+    wav, _ = librosa.effects.trim(wav)
+    return wav.astype(np.float32)
+
+def griffin_lim(spectrogram):
+    '''Applies Griffin-Lim's raw.
+    '''
+    X_best = copy.deepcopy(spectrogram)
+    for i in range(n_iter):
+        X_t = librosa.istft(X_best, hop_length, win_length=win_length, window="hann")
+        est = librosa.stft(X_t, n_fft, hop_length, win_length=win_length)
+        phase = est / np.maximum(1e-8, np.abs(est))
+        X_best = spectrogram * phase
+    X_t = librosa.istft(X_best, hop_length, win_length=win_length, window="hann")
+    y = np.real(X_t)
+    
+    return y
+
+def create_spectrograms():
+    fname = os.path.basename(fpath)
+    mel, mag = wav2spectrograms(fpath)
+    t = mel.shape[0]
+    num_paddings = 3 - (t % 3) if t % 3 != 0 else 0 # for reduction using 3 as reduction factor
+    mel = np.pad(mel, [[0, num_paddings], [0, 0]], mode="constant")
+    mag = np.pad(mag, [[0, num_paddings], [0, 0]], mode="constant")
+    
+    return fname, mel.reshape((-1, n_mels*3)), mag
