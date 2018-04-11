e5b688c32df586914393d6086ef256d522f05fe7
enigma-phi <enigma-phi@users.noreply.github.com>
2018-04-11 17:06:06 -0500

    Fix final bugs

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def train():
 
                     # plot the first alignment for logging
                     align = sess.run(g.alignments)
-                    plot_alignment(align[0], global_step)
+                    plot_alignments(align[0], global_step)
 
 
 if __name__ == '__main__':
