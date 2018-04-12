3a7694f1b15e3c0458d2216f9dd156b67bd67d98
enigma-phi <enigma-phi@users.noreply.github.com>
2018-04-12 01:52:40 -0500

    Session runs now but does not save

--- train.py
+++ train.py
@@ -1,34 +1,28 @@
 from model import Model
 import tensorflow as tf
 from utils import plot_alignments
-from config import LOG_DIR, CHECK_VALS
+from config import CHECK_VALS
 from tqdm import tqdm
 
 def train():
     g = Model()
     print("Graph for training loaded.")
-    
-    with tf.Graph().as_default():
-        # Note: save_summary_secs - number of seconds between computation of summarys
-        # for event log is defaulted to 60 seconds
-        # save_model_secs - number of seconds between the creation of model checkpoints
-        # is defaulted to 600 seconds
 
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
 
-        # Run the session
-        for i in tqdm(range(g.num_batch)):
-            _, g_step = sess.run([g.opt_train, g.global_step])
+    # Run the session
+    for i in tqdm(range(g.num_batch)):
+        _, g_step = sess.run([g.opt_train, g.global_step])
 
-            # write checkpoint files
-            if g_step % CHECK_VALS == 0:
-                # save
-                #sv.saver.save(sess, LOG_DIR + '/step' + str(g_step))
+        # write checkpoint files
+        if g_step % CHECK_VALS == 0:
+            # save
+            #sv.saver.save(sess, LOG_DIR + '/step' + str(g_step))
 
-                # plot the first alignment for logging
-                align = sess.run(g.alignments)
-                plot_alignments(align[0], g_step)
+            # plot the first alignment for logging
+            align = sess.run(g.alignments)
+            plot_alignments(align[0], g_step)
 
 
 if __name__ == '__main__':
